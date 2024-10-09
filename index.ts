c3265ed18991ce34b6a26513bc1e19c6a2ba5d97
turia aspero <turiaaspero@gmail.com>
2024-10-09 14:19:37 +1300

    Begin step 1

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import express from 'express';
+//import schema from DIFFERENT DIRECTORY
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Listening at localhost:${port}`);
+});
