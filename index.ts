2b51d324f9a915a8490927a85e79e371f7bf63fa
turia aspero <turiaaspero@gmail.com>
2024-10-09 16:17:28 +1300

    Access data from  mongoDB... correct schema import path by moving the schema file into the same folder as the server

--- index.ts
+++ index.ts
@@ -1,11 +1,25 @@
 import express from 'express';
-//import schema from DIFFERENT DIRECTORY
+import mongoose from 'mongoose';
+import { Todo, Attachment } from './schema.js';
 
 const app = express();
 const port = 3000;
+const dbName = process.env.NODE_ENV == "test" ? "todos_test" : "todos_dev";
+const uri = `mongodb://localhost:27017/${dbName}`;
+
+async function connectDB() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/todos_test')
+        .catch((err) => console.log(err));
+};
 
 app.use(express.json());
 
-app.listen(port, () => {
+app.get('/todos', async (req, res) => {
+    const data = Todo.find();
+    res.send(await data);
+});
+
+app.listen(port, async () => {
+    await connectDB();
     console.log(`Listening at localhost:${port}`);
 });
