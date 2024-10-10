de38255acf4a85764026e4581edaafce2ece3853
turia aspero <turiaaspero@gmail.com>
2024-10-10 15:44:10 +1300

    Is data persisted?

--- index.ts
+++ index.ts
@@ -6,23 +6,28 @@ const app = express();
 const port = 3000;
 const dbName = process.env.NODE_ENV == "test" ? "todos_test" : "todos_dev";
 const uri = `mongodb://localhost:27017/${dbName}`;
+const db = mongoose.connection;
 
 async function connectDB() {
-    await mongoose.connect(uri)
-        .catch((err) => console.log(err));
+    await mongoose.connect(uri);
 };
 
 app.use(express.json());
 
 app.get('/todos', async (req, res) => {
+
     const data = Todo.find();
+    // console.log(data);
     res.send(await data);
 });
 
 app.post('/todos', (req, res) => {
-    const data = req.body.title;
-    console.log(data);
-    // res.send(data);
+
+    const newTodo = new Todo(req.body);
+
+    newTodo.save();
+    console.log(Todo.find());
+    res.send(JSON.stringify(req.body));
 });
 
 app.listen(port, async () => {
